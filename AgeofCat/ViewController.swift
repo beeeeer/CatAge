4f4b9a61a3c13033e173abf3441cfe4c12e7164e
Adam Nowak <adamcbf@gmail.com>
2015-06-01 12:10:21 +0200

    Initial Commit

---
+++ AgeofCat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeofCat
+//
+//  Created by Adam Nowak on 01.06.2015.
+//  Copyright (c) 2015 NowakAdam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
