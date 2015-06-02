869659ccc8d133e6a0005598f8d0037af02247d2
Adam Nowak <adamcbf@gmail.com>
2015-06-02 11:47:51 +0200

    if statements added

--- AgeofCat/ViewController.swift
+++ AgeofCat/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var catYearLabel: UILabel!
+    
+    @IBOutlet weak var humanYearTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertButton(sender: UIButton) {
+        
+    let yearTextField = Double((humanYearTextField.text as NSString).doubleValue)
+    let catYearsFirstYear:Double = 15
+    let catYearsSecondYear:Double = 24
+        catYearLabel.hidden = false
+        
+     
+        
+    if yearTextField < 2 {
+             catYearLabel.text = "Your cat is " + "\(catYearsFirstYear)" + " human years old"
+        }
+    else if (yearTextField < 3) && (yearTextField > 1)
+        {
+           catYearLabel.text = "Your cat is " + "\(catYearsSecondYear)" + " human years old"
+        }
+    else {
+        var  catYearAddNext = ( yearTextField - 2 ) * 4
+            catYearLabel.text = "Your cat is " + "\(catYearsSecondYear + catYearAddNext)" + " human years old"
+        }
+        
+        
+        
+        
+        
+      
+        humanYearTextField.resignFirstResponder()
+        
+        
+    }
 
 }
 
